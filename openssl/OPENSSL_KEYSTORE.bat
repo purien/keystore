8bd732881b3d63564f32b935713405b0433ba04f
Pascal Urien <pascal.urien@gmail.com>
2021-04-19 09:31:31 +0200

    Add files via upload

---
+++ openssl/OPENSSL_KEYSTORE.bat
@@ -0,0 +1,5 @@
+openssl s_client  -tls1_3  -connect keystore.dyndns.info:443 -groups P-256 -cipher DHE -ciphersuites  TLS_AES_128_CCM_SHA256 -no_ticket -psk 0102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F20
+PAUSE
+
+
+
